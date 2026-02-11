5394619825bec9705ef35abf508438d0c91bf3dc
mangal77102 <mangalsabu2003@gmail.com>
2026-02-11 11:51:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 //complete this code
-class Person {}
+class Person {
+	constuctor(name,age)
+	{
+		this.name=name;
+		this.age=age;
+	}
+	get name()
+	{
+		return this.name;
+		
+	}
+	set age(value)
+	if(value>0)
+	{
+		this.age=value;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constuctor(name,age)
+	{
+		super(name,age)
+	}
+	study()
+	{
+		console.log(`${this.name} is studying`);
+	}
+	
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+
+	constuctor(name,age)
+	{
+		super(name,age)
+	}
+	teach()
+	{
+		console.log(`${this.name} is teaching`);
+	}
+	
+}
 
 // Do not change the code below this line
 window.Person = Person;
